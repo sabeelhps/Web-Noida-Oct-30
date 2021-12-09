1117c1b909c71721b2252f074786aa8341726982
Sabeel Khan <sabeelhps@gmail.com>
2021-12-09 21:18:06 +0530

    Doubt Class 2

---
+++ doubt-class-2/Function&Hoisting/app.js
@@ -0,0 +1,14 @@
+
+
+var a = 100;
+
+function b() {
+    console.log('Inside function b');
+}
+
+
+b();
+console.log(a);
+
+
+
---
+++ doubt-class-2/TDZ/app.js
@@ -0,0 +1,22 @@
+// TDZ - Temporal Dead Zone
+// Only exists for variables declared with let and const
+/////////
+//////////
+/////////
+// console.log(a);//////
+// ////////////////////////////
+// ///////////////////////////
+
+// let a = 100;
+// console.log(a);
+
+// let a = a + 100;
+
+// console.log(a);
+
+
+
+
+
+
+
