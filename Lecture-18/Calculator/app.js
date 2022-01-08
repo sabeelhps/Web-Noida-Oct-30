0bb8144356584d46aa2af6cdbfb000839873e75c
Sabeel Khan <sabeelhps@gmail.com>
2022-01-08 17:23:21 +0530

    Lecture-18

---
+++ Lecture-18/Calculator/app.js
@@ -0,0 +1,55 @@
+const buttons = document.querySelectorAll('button');
+const screen = document.querySelector('#screen');
+
+
+for (const button of buttons) {
+    button.addEventListener('click', function (e) {
+
+        const buttonText = button.innerText.trim();
+
+        // screen.value =screen.value+ buttonText;
+        if (buttonText === 'x') {
+            screen.value += '*';
+        }
+        else if (buttonText === 'C') {
+            screen.value = "";
+        }
+        else if (buttonText === '=') {
+
+            try {
+                // if expression is not evaluated then catch block will be executed
+                screen.value = eval(screen.value);
+            }
+            catch (error) {
+                console.log(error.message);
+                screen.value = 'Invalid Operation';
+            }
+     
+        }
+        else{
+
+            if (screen.value === 'Invalid Operation') {
+                screen.value = "";
+                screen.value += buttonText;
+            } else {
+                screen.value += buttonText;
+            }   
+        }
+        e.stopPropagation();
+    })
+}
+
+
+// document.addEventListener('keydown', function (e) {
+//     if (e.key === 'Enter') {
+//         try {
+//             // if expression is not evaluated then catch block will be executed
+//             screen.value = eval(expression);
+//         }
+//         catch (error) {
+//             console.log(error.message);
+//             screen.value = 'Invalid Operation';
+//         }
+       
+//     }
+// })
---
+++ Lecture-18/snakeGame/app.js
@@ -0,0 +1,42 @@
+const canvas = document.getElementById('canvas');
+const pen = canvas.getContext('2d');
+
+pen.fillStyle = 'yellow';
+
+let intital_x = 0;
+let initial_y = 100;
+
+
+// used to initialise the game with initial properties
+function init() {
+    pen.fillRect(intital_x, initial_y, 49, 49);
+}
+
+
+// draw the updated figures on the canvas
+function draw() {
+    pen.fillRect(intital_x, initial_y, 49,49);
+}
+
+
+// update the game
+
+function update() {
+    intital_x = intital_x + 50;
+}
+
+function gameLoop() {
+    update();
+    draw();
+    console.log('Inside Game Loop');
+}
+
+init();
+
+
+const id = setInterval(gameLoop, 100);
+
+
+
+
+
