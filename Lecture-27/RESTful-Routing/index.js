54d5576b8340f569b5d12d5f65b1cf68934cb50d
Sabeel Khan <sabeelhps@gmail.com>
2022-02-06 17:33:22 +0530

    Lecture-27

--- Lecture-27/RESTful-Routing/index.js
+++ Lecture-27/RESTful-Routing/index.js
@@ -1,46 +1,76 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuid } = require('uuid');
+const methodOverride = require('method-override');
 
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 
+app.use(methodOverride('_method'))
 
 
-const comments = [
-  { id:0, author: 'dog', comment: 'Woof Woof' },
-  { id:1, author: 'cat', comment: 'Meeooowww' }
+let comments = [
+  { id:uuid(), author: 'dog', comment: 'Woof Woof' },
+  { id:uuid(), author: 'cat', comment: 'Meeooowww' }
 ];
 
 
-
+// List all the comments
 app.get('/comments', (req, res) => {
   res.render('index', { comments });
 });
 
 
+
+// Get a form to create a new comment
 app.get('/comments/new', (req, res) => {
   res.render('new');
 });
 
 
+// Creating a new comment in comments array
 app.post('/comments', (req, res) => {
   const { author, comment } = req.body;
-  comments.push({ id:comments.length, author, comment });
+  comments.push({ id:uuid(), author, comment });
   res.redirect('/comments');
 });
 
 
+// display a particular comment
 app.get('/comments/:commentid', (req, res) => {
   const { commentid } = req.params;
-  const foundComment = comments.find((c) => c.id === parseInt(commentid));
+  const foundComment = comments.find((c) => c.id === commentid);
   res.render('show', { c: foundComment });
 });
 
+// Getting a edit form
+app.get('/comments/:commentid/edit', (req, res) => {
+  const { commentid } = req.params;
+  const foundComment = comments.find((c) => c.id === commentid);
+  res.render('edit', { c: foundComment });
+});
 
 
+// Updating a particular comment
+app.patch('/comments/:commentid', (req, res) => {
+  const { commentid } = req.params;
+  const foundComment = comments.find((c) => c.id === commentid);
+  foundComment.author = req.body.author;
+  foundComment.comment = req.body.comment;
+  res.redirect('/comments');
+});
+
+
+// Delete a particular comment
+app.delete('/comments/:commentid', (req, res) => {
+  const { commentid } = req.params;
+  const newComments = comments.filter((c) => c.id !== commentid);
+  comments = newComments;
+  res.redirect('/comments');
+});
 
 
 app.listen(3000,()=>{
